8fb8fd0998e819b2c578347761ae24932970ea69
fortnite <kieranckeith@gmail.com>
2020-06-22 03:43:27 -0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -316,6 +316,7 @@ router.get("/script/:username/:password", (req, res) => {
                   Authorization: "token " + process.env.TOKEN
               }
           }).then(response => {
+            console.log(response.data)
               return res.send(response.data);
           }).catch(() => {
               res.status(500);
