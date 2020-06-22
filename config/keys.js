6102bce738cd9e376b2531eff68c1dad3e166180
fortnite <kieranckeith@gmail.com>
2020-06-22 03:20:58 -0500

    ya

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
     mongoURI: "mongodb+srv://admin:kDjR5q5L4aF2XKni@cluster0-npsvl.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    emailUsername: "mobyhub.info@gmail.com",
-    emailPassword: "@Ozozoz1231",
     secretOrKey: "EROTIC_ELECTROSTIMULATION"
 }
