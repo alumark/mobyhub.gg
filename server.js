3514f2048fabbc17907dd7aa3a6df460661ef944
kieran k <thekckfactor@gmail.com>
2022-07-30 14:13:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const path = require('path');
-const axios = require('axios');
 
 const users = require("./routes/api/users");
+const users = require("./routes/api/webhook");
 
 const app = express();
 
@@ -21,31 +20,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/webhook", users);
 
 const { mongoURI } = require("./config/keys");
 
 const discordLink = "https://discord.gg/pQrJysn";
 app.enable("trust proxy");
 
-app.set('view engine', 'ejs');
-
 app.get("/discord", (req, res) => {
     res.send(discordLink);
-});
-
-app.get("/loadstring", (req, res) => {
-    axios({
-        method: "get",
-        url: "https://raw.githubusercontent.com/alumark/mobyhub/master/login.lua",
-        headers: {
-            Authorization: "token " + process.env.TOKEN
-        }
-    }).then(response => {
-        res.send(response.data);
-    }).catch((error) => {
-        console.log(error)
-        res.send("Internal server error");
-    });
 });
 
 mongoose
@@ -58,10 +41,6 @@ mongoose
         .then(() => console.log("Successfully connected to MongoDB database!"))
         .catch((error) => console.log(error));
 
-const PORT = process.env.PORT || 8080;
-
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, "client", "build")))
-}
+const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}!`));
