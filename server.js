9eab3914afedc182c4b7e9a9c83b15f83c6ac908
moby <thekckfactor@gmail.com>
2022-07-30 14:54:40 -0500

    lolol

--- server.js
+++ server.js
@@ -43,4 +43,8 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, "client", "build")))
+}
+
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}!`));
