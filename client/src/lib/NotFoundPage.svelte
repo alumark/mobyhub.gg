0088e068341569f75be6a9f49156f999f3f75a2f
moby <thekckfactor@gmail.com>
2022-08-02 18:50:45 -0500

    woahhh

--- client/src/lib/NotFoundPage.svelte
+++ client/src/lib/NotFoundPage.svelte
@@ -1,5 +1,7 @@
 <p>
     404 not found
-</p><br/>
+</p>
 
 <a href='/#/' class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>go back home?</a>
+
+<br/>
