392afafc1f5be44f7ac463ab1a650e0fc7a0247a
moby <thekckfactor@gmail.com>
2022-08-02 19:58:55 -0500

    woahhh

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -5,8 +5,6 @@ import jwt_decode from "jwt-decode";
 const stored = localStorage.getItem('jwtToken');
 // or localStorage.getItem('content')
 
-console.log(stored)
-
 // Set the stored value or a sane default.
 export const jwtToken = writable(stored || null);
 export const user = writable(null);
